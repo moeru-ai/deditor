2c188feb29ffccbb83ffe1f537dd5c1e61e6ed85
Neko Ayaka <neko@ayaka.moe>
2025-05-21 12:55:59 +0800

    fix: id

--- src/main/index.ts
+++ src/main/index.ts
@@ -50,7 +50,7 @@ function createWindow(): void {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId('ai.moeru.deditor')
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
