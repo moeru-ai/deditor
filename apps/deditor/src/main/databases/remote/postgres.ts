a1d96adac4f28d564e02bee76d67360c22e2070f
Neko Ayaka <neko@ayaka.moe>
2025-05-22 17:02:02 +0800

    refactor: to monorepo

---
+++ apps/deditor/src/main/databases/remote/postgres.ts
@@ -0,0 +1,34 @@
+import type { PostgresMethods } from '@deditor-app/shared'
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js'
+import type { BrowserWindow } from 'electron'
+
+import { drizzle } from 'drizzle-orm/postgres-js'
+
+import { defineIPCHandler } from '../../electron/define-ipc-handler'
+import { nanoid } from '../../utils/nanoid'
+
+const databaseSessions = new Map<string, PostgresJsDatabase>()
+
+export function registerPostgresJsDatabaseDialect(window: BrowserWindow) {
+  defineIPCHandler<PostgresMethods>(window, 'connectRemoteDatabasePostgres')
+    .handle(async (_, { dsn }) => {
+      const dbSession = drizzle(dsn)
+      const dbSessionId = nanoid()
+      databaseSessions.set(dbSessionId, dbSession)
+
+      // eslint-disable-next-line no-console
+      dbSession.execute('SELECT 1').then(res => console.log(res))
+      return { databaseSessionId: dbSessionId, dialect: 'postgres' }
+    })
+
+  defineIPCHandler<PostgresMethods>(window, 'queryRemoteDatabasePostgres')
+    .handle(async (_, { databaseSessionId, statement }) => {
+      if (!databaseSessions.has(databaseSessionId)) {
+        throw new Error('Database session ID not found in session map, please connect to the database first.')
+      }
+
+      const dbSession = databaseSessions.get(databaseSessionId)!
+      const res = await dbSession.execute(statement)
+      return { databaseSessionId, results: res }
+    })
+}
---
+++ apps/deditor/src/renderer/src/composables/databases/remote/postgres.ts
@@ -0,0 +1,32 @@
+import type { PostgresMethods } from '@deditor-app/shared'
+
+import { ref } from 'vue'
+
+import { defineClientMethod } from '../../electron/define-client-method'
+
+export function useRemotePostgres() {
+  const methods = <TMethod extends keyof PostgresMethods>(method: TMethod) => defineClientMethod<PostgresMethods, TMethod>(method)
+  const databaseSessionId = ref<string>()
+
+  return {
+    connect: async (dsn: string) => {
+      const id = await methods('connectRemoteDatabasePostgres').call({ dsn })
+      databaseSessionId.value = id.databaseSessionId
+
+      return id
+    },
+    execute: async <R = Record<string, unknown>>(statement: string, parameters: any[] = []): Promise<R[]> => {
+      if (!databaseSessionId.value) {
+        throw new Error('Database session ID is not set. Please connect to a database first.')
+      }
+
+      const res = await methods('queryRemoteDatabasePostgres').call({
+        databaseSessionId: databaseSessionId.value!,
+        statement,
+        parameters,
+      })
+
+      return res.results as R[]
+    },
+  }
+}
