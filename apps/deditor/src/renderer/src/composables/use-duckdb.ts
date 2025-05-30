2a90d3ff397a18aec4fe3a38e7507be40613be64
Neko Ayaka <neko@ayaka.moe>
2025-05-30 13:09:14 +0800

    refactor: use duckdb-wasm's new queryWithColumns helper function

--- apps/deditor/src/renderer/src/composables/use-duckdb.ts
+++ apps/deditor/src/renderer/src/composables/use-duckdb.ts
@@ -1,21 +1,39 @@
-import type { ConnectOptions, DuckDBWasmDrizzleDatabase } from '@proj-airi/drizzle-duckdb-wasm'
-import type { MaybeRefOrGetter } from 'vue'
+import type { ArrowField, ConnectOptions, DuckDBWasmDrizzleDatabase, ResultColumns } from '@proj-airi/drizzle-duckdb-wasm'
+import type { MaybeRefOrGetter, Ref } from 'vue'
 
 import { drizzle } from '@proj-airi/drizzle-duckdb-wasm'
 import { getImportUrlBundles } from '@proj-airi/drizzle-duckdb-wasm/bundles/import-url-browser'
 import { onMounted, onUnmounted, ref, toValue, watch } from 'vue'
 
 export function useDuckDB(options?: ConnectOptions & { autoConnect?: boolean }) {
+  const connecting = ref(false)
   const db = ref<DuckDBWasmDrizzleDatabase>()
   const closeFunc = ref<() => Promise<void>>(async () => { })
+  const errored = ref<boolean>(false)
+  const error = ref<unknown>()
 
   async function connect() {
-    const drizzleClient = await drizzle({ connection: { ...options, bundles: getImportUrlBundles() } })
-    db.value = drizzleClient
+    connecting.value = true
 
-    closeFunc.value = async () => {
-      const c = await drizzleClient?.$client
-      c?.close()
+    try {
+      const drizzleClient = await drizzle({ connection: { ...options, bundles: getImportUrlBundles() } })
+      db.value = drizzleClient
+
+      closeFunc.value = async () => {
+        (await drizzleClient?.$client).close()
+      }
+    }
+    catch (err) {
+      console.error('Error connecting to DuckDB:', err)
+
+      errored.value = true
+      error.value = err
+
+      db.value = undefined
+      closeFunc.value = async () => { }
+    }
+    finally {
+      connecting.value = false
     }
   }
 
@@ -32,35 +50,82 @@ export function useDuckDB(options?: ConnectOptions & { autoConnect?: boolean }) 
   return {
     connect,
     db,
+    connecting,
+    errored,
+    error,
   }
 }
 
 export function useDuckDBQuery(queryStr: MaybeRefOrGetter<string>, options?: { autoConnect?: boolean, immediate?: boolean } & ConnectOptions) {
   const result = ref<Record<string, unknown>[]>()
+  const resultColumns = ref<ArrowField[]>([])
   const errored = ref<boolean>(false)
   const error = ref<unknown>()
+  const querying = ref(false)
 
-  const duckDB = useDuckDB(options)
+  const { db, connecting, error: dbError, errored: dbErrored } = useDuckDB(options)
+
+  async function _query(query: string, params: unknown[] = []): Promise<ResultColumns> {
+    if (!db.value) {
+      return {
+        rows: [],
+        columns: [],
+        _results: undefined as any,
+        _schema: undefined as any,
+      }
+    }
+
+    return await (await db.value!.$client).queryWithColumns(query, params)
+  }
 
   async function query() {
+    querying.value = true
+
     try {
-      result.value = await duckDB.db.value?.execute(toValue(queryStr))
+      const results = await _query(toValue(queryStr))
+
       errored.value = false
       error.value = undefined
+
+      result.value = results.rows
+      resultColumns.value = results.columns
     }
     catch (err) {
       errored.value = true
       error.value = err
     }
+    finally {
+      querying.value = false
+    }
   }
 
   onMounted(async () => options?.immediate && await query())
   watch(() => toValue(queryStr), () => query())
+  watch(db, async (newDb) => {
+    if (newDb && options?.immediate) {
+      await query()
+    }
+  })
+  watch(dbError, (newError) => {
+    if (newError) {
+      errored.value = true
+      error.value = newError
+    }
+  })
+  watch(dbErrored, (newErrored) => {
+    if (newErrored) {
+      errored.value = true
+      error.value = dbError.value
+    }
+  })
 
   return {
     result,
+    resultColumns: resultColumns as Ref<ArrowField[]>,
     error,
     errored,
     execute: query,
+    querying,
+    connecting,
   }
 }
